8a1c43c5d830cc4228e8fe7c8cf1b894c2c93d45
reshmanayak-1999 <reshmanayak1999@gmail.com>
2025-04-05 10:31:17 +0530

    Add project files.

---
+++ C# Nunit Project/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace C__Nunit_Project
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
