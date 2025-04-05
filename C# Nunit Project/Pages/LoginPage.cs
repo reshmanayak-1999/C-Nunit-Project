a3d166346f94e31de95e5bc17e06ef2d431672da
reshmanayak-1999 <reshmanayak1999@gmail.com>
2025-04-05 11:20:01 +0530

    fixed the pagefactory initialization

--- C# Nunit Project/Pages/LoginPage.cs
+++ C# Nunit Project/Pages/LoginPage.cs
@@ -10,6 +10,7 @@ namespace C__Nunit_Project.Pages
 {
     public class LoginPage
     {
+        
         [FindsBy(How = How.Id, Using = "username")]
         private IWebElement Username { get; set; }
 
@@ -21,6 +22,7 @@ namespace C__Nunit_Project.Pages
 
         public LoginPage(IWebDriver driver)
         {
+           
             PageFactory.InitElements(driver, this);
         }
 
